3ab427a1b78f5fd44aa0b572c7e2e9dd85a2a816
Eager (Emilien) <emilien.pons@viacesi.fr>
2018-02-07 16:08:10 +0100

    Mission-A4- Ajout Algorithme

--- MissionA4.py
+++ MissionA4.py
@@ -8,9 +8,21 @@ img2 = cv2.imread('Images/Jupiter2.pbm',0)
 #Texture Splatting / Blending
 
 ##Version 1 - Classique ajout
-
+'''
 ##Nécessite matrice même taille
-img3 = cv2.addWeighted(img, 0.5, img2, 0.5, 0)
+imgF = cv2.addWeighted(img, 0.5, img2, 0.5, 0)
+'''
 
-cv2.imshow('Rendu Net',img3)
+###Version 2 - Algo
+
+isize = img.shape  #Propriétés de l'images
+isize2 = img2.shape  #Propriétés de l'images
+imgF = img
+for i in range(0, isize[0]):
+    for j in range(0, isize[1]):
+        p1 = img[i][j]
+        p2 = img2[i][j]
+        imgF[i][j] = min(p1,p2)#On privilégie le noir avec le min
+
+cv2.imshow('Rendu Net',imgF)
 cv2.waitKey(0)
