25128dfb9e439549f979ea32a130c8927fc23224
Eager (Emilien) <emilien.pons@viacesi.fr>
2018-02-15 14:17:31 +0100

    Optimisation du code U1

--- MissionU1.py
+++ MissionU1.py
@@ -1,57 +1,52 @@
-# import the necessary packages
-from imutils import paths
-import argparse
 import numpy as np
 from matplotlib import pyplot as plt
 import cv2
 
 
 ##Une deuxième solution pour déterminer le niveau de blur, en plus de la transformation de Fourrier
-##Permet de déterminer le niveau de fréquence avec une valeur numérique
+#Petite fonction qui retourne la mesure de Laplacian (Valeur numérique), càd le niveau de fréquence dans une valeur numérique
 def variance_of_laplacian(image):
-    # compute the Laplacian of the image and then return the focus
-    # measure, which is simply the variance of the Laplacian
     return cv2.Laplacian(image, cv2.CV_64F).var()
 
 image = cv2.imread("Images/U1_surface.pbm")
 gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-
 fm = variance_of_laplacian(gray)
 
-# if the focus measure is less than the supplied threshold,
-# then the image should be considered "blurry"
+# Si la valeur est inférieure à 100, l'image est considérée comme "blurr"
 if fm < 100:
-    text = "Blurry"
+    text = "blurr"
 else:
-    text = "Not Blurry"
+    text = "pas blurr"
 print("L'image est", text, "avec : ", fm)
 
 image = cv2.imread("Images/U1_surface.pbm",0)
-##Filtres pour détecter les contours
+
+#(opt) retirer le bruit
+#image = cv2.GaussianBlur(image,(3,3),0)
+
+#V1 - Filtre Laplacian
 img_laplacian = cv2.Laplacian(image,cv2.CV_64F)
 
-#canny
+#V2 - Filtre Canny
 img_canny = cv2.Canny(image,100,200)
 
-#sobel
-img_sobelx = cv2.Sobel(image,cv2.CV_8U,1,0,ksize=5)
-img_sobely = cv2.Sobel(image,cv2.CV_8U,0,1,ksize=5)
+#V3 - Filtre Laplacian
+img_sobelx = cv2.Sobel(image,cv2.CV_64F,1,0,ksize=5)
+img_sobely = cv2.Sobel(image,cv2.CV_64F,0,1,ksize=5)
 img_sobel = img_sobelx + img_sobely
 
-
-#prewitt
+#V4 - Filtre Prewitt
 kernelx = np.array([[1,1,1],[0,0,0],[-1,-1,-1]])
 kernely = np.array([[-1,0,1],[-1,0,1],[-1,0,1]])
 img_prewittx = cv2.filter2D(image, -1, kernelx)
 img_prewitty = cv2.filter2D(image, -1, kernely)
 
-#Hough - Retoure un array (pixel / radians) - Pas performant sur l'image
-'''
-img_houghcircle = image
+#V5 - Filtre Hough (Détection)
+img_houghcircle = image.copy()
+img_hough = image.copy()
 
 edges = cv2.Canny(img_hough,50,150,apertureSize = 3)
-
-lines = cv2.HoughLines(edges,1,np.pi/180,200)
+lines = cv2.HoughLines(edges,1,np.pi/180,200) ##On traite sur une canny
 for rho,theta in lines[0]:
     a = np.cos(theta)
     b = np.sin(theta)
@@ -61,25 +56,20 @@ for rho,theta in lines[0]:
     y1 = int(y0 + 1000*(a))
     x2 = int(x0 - 1000*(-b))
     y2 = int(y0 - 1000*(a))
-
     cv2.line(img_hough,(x1,y1),(x2,y2),(0,0,255),2)
 
-
 circles = cv2.HoughCircles(img_houghcircle,cv2.HOUGH_GRADIENT,1,20,param1=50,param2=30,minRadius=0,maxRadius=0)
-
 circles = np.uint16(np.around(circles))
-for i in circles[0,:]:
-    # draw the outer circle
-    cv2.circle(img_houghcircle,(i[0],i[1]),i[2],(0,255,0),2)
-    # draw the center of the circle
-    cv2.circle(img_houghcircle,(i[0],i[1]),2,(0,0,255),3)
 
+for i in circles[0,:]:
+    cv2.circle(img_houghcircle,(i[0],i[1]),i[2],(0,255,0),2)
+    cv2.circle(img_houghcircle,(i[0],i[1]),2,(0,0,255),3)
 
 cv2.imshow("img_hough",img_hough)
 cv2.imshow("img_houghcircle",img_houghcircle)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
-'''
+
 
 plt.subplot(131),plt.imshow(image, cmap = 'gray')
 plt.title('Original Image'), plt.xticks([]), plt.yticks([])
