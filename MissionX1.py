c51ef04ea60a5aec4e2bf327707939b2e46c8eee
Eager (Emilien) <emilien.pons@viacesi.fr>
2018-02-15 13:56:29 +0100

    Optimisation du code X1

--- MissionX1.py
+++ MissionX1.py
@@ -2,18 +2,6 @@ import numpy as np
 import cv2
 from matplotlib import pyplot as plt
 
-
-##Ouverture depuis un .dat -> :(
-'''
-#_io.TextIOWrapper
-test_file = open('Images/Asellus Secundus.dat', 'r', encoding='utf-8', errors='replace')
-# Travailler avec les streams ==> https://docs.python.org/3/library/io.html
-test_lines = test_file.readlines()
-print(test_lines)
-#cv2.imshow('image',test_lines)
-test_file.close()
-'''
-
 #Principe de Fourier -> DFT : Discrete Fourier Transfort
 ''' La transformation de fourier s'exprime comme une somme infinie des fonctions tigonométriques de toutes fréquences / pulsations.
 On l'utilise pour étudier des domaines de fréquence.
@@ -21,12 +9,11 @@ On passe d'un signal à un spectre grâce à sa transformation.
 Exemple de fourier : https://upload.wikimedia.org/wikipedia/commons/e/e8/Periodic_identity_function.gif
 
 Si l'amplitude varie dans un temps court, c'est un haut niveau de fréquence. 
-Dans une image, plus le spectre affiche du bruit et des espacements, plus les fréquences sont hautes
+Dans une image, plus le spectre affiche du bruit et des espacements, plus les fréquences sont hautes.
+FFT est un filtre passe haut, car il permet de connaître la fréquence de chaque kernel block ,et par quelle région il passe. Ainsi chaque kernel peut être passe haut ou passe bas.
 '''
 
 img = cv2.imread('Images/kamino.jpeg',0)
-
-#FFT est un filtre passe haut, car il permet de connaître la fréquence de chaque kernel block ,et par quelle région il passe. Ainsi chaque kernel peut être passe haut ou passe bas.
 
 #Transformation de fourier -> FFT : Fast Fourier Transform
 f = np.fft.fft2(img) ##Nous donne la fréquence de transformation (array compliqué) appellé "GrayScale"
@@ -55,8 +42,3 @@ plt.title('Coming Back'), plt.xticks([]), plt.yticks([])
 plt.subplot(144),plt.imshow(img_back) ##Si on ne met pas de gray - ringing effects (à cause de la fenêtre)
 plt.title('Résultat JET'), plt.xticks([]), plt.yticks([])
 plt.show()
-
-#C'est une fonction et non une matrice, on ne peut donc pas utiliser de imgshow.
-#cv2.imshow('STD',img_back)
-#cv2.waitKey(0)
-#cv2.destroyAllWindows()
